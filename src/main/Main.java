9488f5508555e8d03861ccb339cf82095eb4ee6e
Kyler Davis <ky.s.dav@gmail.com>
2018-08-30 12:38:38 -0600

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,33 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        String userLanguage = Locale.getDefault().getLanguage();
+        System.out.println(userLanguage);
+        Locale  userDefault = new Locale(userLanguage);
+        Locale.setDefault(userDefault);
+        ResourceBundle defaultBundle = ResourceBundle.getBundle("Resources", userDefault);
+
+        //Locale userLocale = new Locale.Builder().setLanguage(Locale.getDefault().getLanguage()).build();
+        //Locale locale = Locale.userLocale;
+        //ResourceBundle userBundle = ResourceBundle.getBundle(userLocale, locale);
+
+        Parent root = FXMLLoader.load(getClass().getResource("../View/LoginScreen.fxml"),defaultBundle);
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
