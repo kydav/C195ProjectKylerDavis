a23360e51a608b9201388ba6fa7d7c4824e72042
Kyler Davis <ky.s.dav@gmail.com>
2018-08-31 09:15:08 -0600

    Some updates for login management

--- src/Controller/LoginScreenController.java
+++ src/Controller/LoginScreenController.java
@@ -1,6 +1,8 @@
 package Controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import java.io.IOException;
 import javafx.event.ActionEvent;
@@ -19,24 +21,40 @@ public class LoginScreenController {
     @FXML
     private TextField loginUsername;
     @FXML
-    private TextField loginPassword;
+    private PasswordField loginPassword;
     @FXML
     void loginClicked(ActionEvent event) throws IOException, ClassNotFoundException, SQLException, Exception {
         String userName = loginUsername.getText();
+        //System.out.println(loginPassword.getText());
         String password = loginPassword.getText();
-        String query = "SELECT password FROM user WHERE userName = '" + userName + "'";
 
+
+        if(password == "" || userName == ""){
+
+        };
+
+        String query = "SELECT password FROM user WHERE userName = '" + userName + "'";
         Statement stmt = conn.createStatement();
         ResultSet rs = stmt.executeQuery(query);
 
-        rs.next();
-        String userPass = rs.getString("password");
-
-        if(userPass.equals(password)){
-            System.out.println("yay!");
+        //For when username doesn't exist and query result set is empty
+        if(!rs.next()){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("%login.userNotFound");
+            alert.setHeaderText("User " + userName + " not found");
+            alert.showAndWait();
         }
+        //For when username exists and password is incorrect
+        else if(){
 
+        } else{
+            //rs.next();
+            String userPass = rs.getString("password");
+            if (userPass.equals(password)) {
+                System.out.println("yay!");
+            }
 
+        }
 
 
     }
