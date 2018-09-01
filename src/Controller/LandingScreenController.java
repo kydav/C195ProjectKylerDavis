adc974d8405704671e4a0b390b76855ed244fa40
Kyler Davis <ky.s.dav@gmail.com>
2018-08-31 18:57:26 -0600

    some more updates

---
+++ src/Controller/LandingScreenController.java
@@ -0,0 +1,33 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+
+public class LandingScreenController {
+
+    @FXML
+    private Button schedulingButton;
+
+    @FXML
+    private Button customersButton;
+
+    @FXML
+    private Button reportsButton;
+
+    @FXML
+    void manageCustomersClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void reportsButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void schedulingButtonClicked(ActionEvent event) {
+
+    }
+
+}
--- src/Controller/LoginScreenController.java
+++ src/Controller/LoginScreenController.java
@@ -6,56 +6,60 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import java.io.IOException;
 import javafx.event.ActionEvent;
-import main.DBConnection;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ResourceBundle;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 
 import static main.DBConnection.*;
 
 
 public class LoginScreenController {
     @FXML
+    private ResourceBundle resources;
+    @FXML
     private TextField loginUsername;
     @FXML
     private PasswordField loginPassword;
     @FXML
+    private Button loginButton;
+    @FXML
     void loginClicked(ActionEvent event) throws IOException, ClassNotFoundException, SQLException, Exception {
         String userName = loginUsername.getText();
-        //System.out.println(loginPassword.getText());
         String password = loginPassword.getText();
+        if (userName.trim().isEmpty() || password.trim().isEmpty()) {
+            alertFunction(resources.getString("login.fieldsEmpty"), resources.getString("login.empty"));
+        } else {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT password FROM user WHERE userName = '" + userName + "'");
+            if (!rs.next()) {
+                alertFunction(resources.getString("login.userNotFound"), resources.getString("login.user") + " " + userName + " " + resources.getString("login.notFound"));
+            } else {
+                String userPass = rs.getString("password");
+                if (userPass.equals(password)) {
+                    System.out.println("User login successful");
 
+                    Stage stage = (Stage) loginButton.getScene().getWindow();
+                    Parent login = FXMLLoader.load(getClass().getResource("LandingScreen.fxml"));
+                    Scene scene = new Scene(login);
+                    stage.setScene(scene);
+                    stage.show();
 
-        if(password == "" || userName == ""){
-
-        };
-
-        String query = "SELECT password FROM user WHERE userName = '" + userName + "'";
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery(query);
-
-        //For when username doesn't exist and query result set is empty
-        if(!rs.next()){
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("%login.userNotFound");
-            alert.setHeaderText("User " + userName + " not found");
-            alert.showAndWait();
-        }
-        //For when username exists and password is incorrect
-        else if(){
-
-        } else{
-            //rs.next();
-            String userPass = rs.getString("password");
-            if (userPass.equals(password)) {
-                System.out.println("yay!");
+                } else {
+                    alertFunction(resources.getString("login.incorrectPassword"), resources.getString("login.incorrect") + " " + userName + ". " + resources.getString("login.tryAgain"));
+                }
             }
-
         }
-
-
+    }
+    void alertFunction(String title, String header){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.showAndWait();
     }
 }
